ae059e75ac535fd866fb4cdafbf4976d561b5c75
Study Hsueh <ph.study@gmail.com>
2020-04-21 00:58:54 +0800

    Bump to v0.3.1

--- src/main/java/io/tenmax/cqlkit/Consts.java
+++ src/main/java/io/tenmax/cqlkit/Consts.java
@@ -1,5 +1,5 @@
 package io.tenmax.cqlkit;
 
 public class Consts {
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = Consts.class.getPackage().getImplementationVersion();
 }
